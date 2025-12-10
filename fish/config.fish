525097e037c22113f3673cf652ccd30d0e19d72c
raihandotim <hello@raihan.im>
2025-12-10 16:53:10 +0000

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+   set -U fish_greeting "         _ _                   _           
+ _ __ __ _(_) |__   __ _ _ __   (_)_ __ ___  
+| '__/ _` | | '_ \ / _` | '_ \  | | '_ ` _ \ 
+| | | (_| | | | | | (_| | | | |_| | | | | | |
+|_|  \__,_|_|_| |_|\__,_|_| |_(_)_|_| |_| |_|
+                                             "
+end
